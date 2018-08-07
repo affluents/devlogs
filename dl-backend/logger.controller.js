56f69d95eab1bdcea26f499776d7aff665bcaa32
Preetham Goud <preethamsureshgoud@gmail.com>
2018-08-07 10:03:08 +0530

    Basic Backend

--- dl-backend/logger.controller.js
+++ dl-backend/logger.controller.js
@@ -1,7 +1,21 @@
+const saveDoc = require('./database.controller').saveDoc;
+const getDocs = require('./database.controller').getDocs;
+
 function saveLog(logObj) {
-    console.log(logObj);
+    const obj = {};
+    obj.title = logObj.title;
+    obj.exc = logObj.exc;
+    obj.severity = logObj.severity;
+    var d = new Date();
+    obj.date = d;
+    saveDoc(obj);
+}
+
+function getLogs(callback) {
+    getDocs("logs", callback);
 }
 
 module.exports = {
-    saveLog: saveLog
+    saveLog: saveLog,
+    getLogs: getLogs
 }
